1eebb55fb4fd5e83f6c34a00d2f4ea7e97166d20
Mahdi Hasan Emran <ahmedemran412@gmail.com>
2024-10-28 02:01:34 +0600

    LONG TIME NO SEE 2.01 AM

---
+++ 2033A_Sakurako and Kosuke.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a;
+    while(a--){
+        cin>>b;
+        if(b%2==0){
+            cout<<"Sakurako"<<endl;
+        }
+        else{
+            cout<<"Kosuke"<<endl;
+        }
+    }
+}
