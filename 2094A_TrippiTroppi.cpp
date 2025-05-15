7e385872f7ef5c50b6e0b4ec5c2b3d75512cace9
eeemrann <ahmedemran412@gmail.com>
2025-05-15 14:00:53 +0600

    2.00 PM

---
+++ 2094A_TrippiTroppi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long t; 
+    cin >> t;
+    while(t--){
+        string a, b, c; 
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+
+}
